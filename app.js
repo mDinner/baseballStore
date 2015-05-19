573bfe4b29a41a3b779a3de95b004fe07382dd11
Tyler Dinner <tylerdinner@gmail.com>
2015-05-18 22:12:19 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var monSess = require('mongoose-session');
+var path = require('path');
+
+var Item = require('./app/models/item.js');
+
+// app config
+app.use(bodyParser());
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, './app/views'));
+app.use(express.static(__dirname + '/app/public'));
+
+// route section
+var router = express.Router();
+require( './app/routes' )( router, EmailList, User, Post, Show, bcrypt );
+// Register router, to prefix all routes w/ '/api' use: app.use('/api', router);
+app.use(router);
+mongoose.connect('mongodb://tyler:daylite@novus.modulusmongo.net:27017/vesuh6yD');
+//mongoose.connect('mongodb://localhost/test');
+var port = process.env.PORT || 8008;
+app.listen(port);
+console.log('now serving on port ' + port);
